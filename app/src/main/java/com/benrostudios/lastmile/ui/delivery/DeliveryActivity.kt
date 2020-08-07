00d8f5788186c194c31caa60ff49c9265b38d332
Hemanth Krishna <hkpdev008@gmail.com>
2020-08-07 07:36:36 +0530

    chore: add resources

---
+++ app/src/main/java/com/benrostudios/lastmile/ui/delivery/DeliveryActivity.kt
@@ -0,0 +1,12 @@
+package com.benrostudios.lastmile.ui.delivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.benrostudios.lastmile.R
+
+class DeliveryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delivery)
+    }
+}
