0759894245e038211ea74f32871d79e5e8f07d03
Hemanth Krishna <hkpdev008@gmail.com>
2020-08-07 14:49:17 +0530

    feat: bottomNavBars

--- app/src/main/java/com/benrostudios/lastmile/ui/client/ClientActivity.kt
+++ app/src/main/java/com/benrostudios/lastmile/ui/client/ClientActivity.kt
@@ -2,11 +2,19 @@ package com.benrostudios.lastmile.ui.client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.benrostudios.lastmile.R
+import kotlinx.android.synthetic.main.activity_client.*
 
 class ClientActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client)
+        navController = findNavController(R.id.nav_host_fragment_home_activity)
+        bottom_navigation_view_client_activity.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/benrostudios/lastmile/ui/delivery/DeliveryActivity.kt
+++ app/src/main/java/com/benrostudios/lastmile/ui/delivery/DeliveryActivity.kt
@@ -2,11 +2,19 @@ package com.benrostudios.lastmile.ui.delivery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.benrostudios.lastmile.R
+import kotlinx.android.synthetic.main.activity_client.*
+import kotlinx.android.synthetic.main.activity_delivery.*
 
 class DeliveryActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_delivery)
+        navController = findNavController(R.id.nav_host_fragment_home_activity)
+        bottom_navigation_view_delivery_activity.setupWithNavController(navController)
     }
 }
